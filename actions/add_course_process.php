f0e8ae3f8ba95b2b2b4332894a44bfd67fdc6142
Jakub Malik <malik.j@poczta.fm>
2023-11-28 19:07:20 +0100

    prcess

--- actions/add_course_process.php
+++ actions/add_course_process.php
@@ -4,7 +4,7 @@ session_start();
 
 // Sprawdzenie, czy użytkownik jest zalogowany
 if (!isset($_SESSION['username'])) {
-    header("Location: login.php");
+    header("Location: /phpsql/pages/login.php");
     exit;
 }
 
@@ -20,20 +20,20 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $title = $_POST['title'];
     $description = $_POST['description'];
-    $instructor = $_POST['instructor'];
-    $username = $_SESSION['username'];
+    $creator_id = $_SESSION['user_id']; // Pobierz ID twórcy kursu z sesji
 
-$insertSql = "INSERT INTO courses (title, description, instructor) VALUES ('$title', '$description', '$instructor')";
+    $insertSql = "INSERT INTO courses (title, description, creator_id) VALUES ('$title', '$description', '$creator_id')";
 
-if ($conn->query($insertSql) === TRUE) {
-    // Pomyślne dodanie kursu, przekieruj na stronę z kursami
+    if ($conn->query($insertSql) === TRUE) {
+        // Pomyślne dodanie kursu, przekieruj na stronę z kursami
+        header("Location: /phpsql/pages/browse_courses.php");
+        exit;
+    } else {
+        // Błąd podczas dodawania kursu
+        $error_message = "Błąd podczas dodawania kursu. Spróbuj ponownie.";
+    }
+    // Przekieruj na stronę z kursami w przypadku błędu
     header("Location: /phpsql/pages/browse_courses.php");
-    exit;
-} else {
-    // Błąd podczas dodawania kursu
-    $error_message = "Błąd podczas dodawania kursu. Spróbuj ponownie.";
-}
-    header("Location: /phpsql/pages/courses.php");
 }
 
 $conn->close();
