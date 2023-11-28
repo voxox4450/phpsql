c36ec9f639247a243b2c1afaa09b706f02025a8b
voxox4450 <voxoxgaming@gmail.com>
2023-11-28 19:13:30 +0100

    .

--- pages/add_course.php
+++ pages/add_course.php
@@ -12,15 +12,6 @@ if (!isset($_SESSION['username'])) {
 // Pobierz informacje o zalogowanym użytkowniku (możesz pobierać więcej informacji z bazy danych, jeśli potrzebujesz)
 $username = $_SESSION['username'];
 
-// Połączenie z bazą danych
-include 'phpsql/settings.php';
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
 // Poniżej możesz umieścić kod HTML/CSS, który wyświetli panel użytkownika
 ?>
 
@@ -29,7 +20,7 @@ if ($conn->connect_error) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Dodaj Kurs</title>
 </head>
 <body>
@@ -37,15 +28,15 @@ if ($conn->connect_error) {
 
     <div class="container">
         <h2>Dodaj nowy kurs</h2>
-        <form action="/phpsql/actions/add_course_process.php" method="post">
+        <form action="add_course_process.php" method="post">
             <label for="title">Tytuł kursu:</label>
             <input type="text" name="title" required>
 
             <label for="description">Opis kursu:</label>
             <textarea name="description" rows="4" required></textarea>
 
-            <!-- Użyj ukrytego pola, aby przekazać ID twórcy kursu -->
-            <input type="hidden" name="creator_id" value="<?php echo $_SESSION['user_id']; ?>">
+            <label for="instructor">Instruktor:</label>
+            <input type="text" name="instructor" required>
 
             <button type="submit">Dodaj kurs</button>
         </form>
