f0f6849375dd7f9b7d61924f77360915f9e0ded0
Jakub Malik <malik.j@poczta.fm>
2023-11-29 21:28:51 +0100

    hasłowanie

--- pages/change_password.php
+++ pages/change_password.php
@@ -9,13 +9,13 @@ if (!isset($_SESSION['username'])) {
     exit;
 }
 
-// Połączenie z bazą danych (zakładam, że masz już plik settings.php z konfiguracją)
+// Dołączenie pliku z ustawieniami bazy danych
 include '../settings.php';
 
-// Pobierz informacje o zalogowanym użytkowniku
+// Pobranie informacji o zalogowanym użytkowniku
 $username = $_SESSION['username'];
 
-
+// Sprawdzenie połączenia z bazą danych
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
@@ -32,45 +32,7 @@ if ($result->num_rows > 0) {
     exit;
 }
 
-// Obsługa formularza zmiany hasła
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Pobierz dane z formularza
-    $currentPassword = $_POST['current_password'];
-    $newPassword = $_POST['new_password'];
-    $confirmNewPassword = $_POST['confirm_new_password'];
-
-    // Sprawdź, czy aktualne hasło jest poprawne
-    if (password_verify($currentPassword, $row['password'])) {
-        // Aktualne hasło jest poprawne
-        if ($newPassword === $confirmNewPassword) {
-            // Nowe hasło zostało potwierdzone
-            $hashedNewPassword = password_hash($newPassword, PASSWORD_DEFAULT);
-
-            // Aktualizuj hasło w bazie danych (parametryzowane zapytanie)
-            $updateSql = "UPDATE users SET password=? WHERE id=?";
-            $stmt = $conn->prepare($updateSql);
-            $stmt->bind_param("si", $hashedNewPassword, $userId);
-
-            if ($stmt->execute()) {
-                // Aktualizacja hasła zakończona sukcesem
-                header("Location: /phpsql/pages/profile.php");
-                exit;
-            } else {
-                // Błąd podczas aktualizacji hasła
-                $error_message = "Błąd podczas aktualizacji hasła. Spróbuj ponownie.";
-            }
-        } else {
-            // Nowe hasła nie pasują do siebie
-            $error_message = "Nowe hasło i jego potwierdzenie muszą być identyczne.";
-        }
-    } else {
-        // Aktualne hasło jest niepoprawne
-        $error_message = "Aktualne hasło jest niepoprawne.";
-    }
-}
-
-
-$conn->close();
+include '../includes/header.php';
 ?>
 
 <!DOCTYPE html>
@@ -82,8 +44,6 @@ $conn->close();
     <title>Zmień Hasło</title>
 </head>
 <body>
-    <?php include '../includes/header.php'; ?>
-
     <div class="container">
         <h2>Zmień Hasło</h2>
 
@@ -93,7 +53,7 @@ $conn->close();
         }
         ?>
 
-        <form action="change_password.php" method="post">
+        <form action="change_password_process.php" method="post">
             <label for="current_password">Aktualne Hasło:</label>
             <input type="password" name="current_password" required>
 
@@ -110,3 +70,7 @@ $conn->close();
     <?php include '../includes/footer.php'; ?>
 </body>
 </html>
+
+<?php
+$conn->close();
+?>
