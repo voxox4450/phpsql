807434c82d74a053a3af3ff0ea25cb74940ac48e
Jakub Malik <malik.j@poczta.fm>
2023-11-29 13:32:14 +0100

    d

---
+++ pages/start_course.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+
+include('../settings.php');
+
+// Sprawdź, czy użytkownik jest zalogowany
+if (!isset($_SESSION['user_id'])) {
+    header("Location: /phpsql/pages/login.php");
+    exit;
+}
+
+// Sprawdź, czy przesłano poprawne dane POST
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['course_id'])) {
+    $user_id = $_SESSION['user_id'];
+    $course_id = $_POST['course_id'];
+
+    // Sprawdź, czy użytkownik już ukończył ten kurs
+    $checkCompletionSql = "SELECT * FROM completed_courses WHERE user_id=$user_id AND course_id=$course_id";
+    $result = $conn->query($checkCompletionSql);
+
+    if ($result->num_rows == 0) {
+        // Użytkownik jeszcze nie ukończył kursu, dodaj do tabeli completed_courses
+        $insertCompletionSql = "INSERT INTO completed_courses (user_id, course_id, completion_date) VALUES ($user_id, $course_id, CURDATE())";
+        if ($conn->query($insertCompletionSql)) {
+            echo "Kurs został rozpoczęty.";
+        } else {
+            echo "Błąd podczas rozpoczynania kursu: " . $conn->error;
+        }
+    } else {
+        // Użytkownik już ukończył kurs, zaktualizuj datę ukończenia
+        $updateCompletionSql = "UPDATE completed_courses SET completion_date = CURDATE() WHERE user_id=$user_id AND course_id=$course_id";
+        if ($conn->query($updateCompletionSql)) {
+            echo "Kurs został ponownie ukończony.";
+        } else {
+            echo "Błąd podczas ponownego ukończania kursu: " . $conn->error;
+        }
+    }
+} else {
+    echo "Błędne dane przesłane do formularza.";
+}
+
+$conn->close();
+?>
--- pages/view_course.php
+++ pages/view_course.php
@@ -8,90 +8,90 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['id'])) {
     $course_id = $_GET['id'];
 
     // Pobierz dane kursu na podstawie ID
-    $selectCourseSql = "SELECT * FROM courses WHERE id=$course_id";
+    $selectCourseSql = "SELECT * FROM courses WHERE id={$course_id}";
     $courseResult = $conn->query($selectCourseSql);
 
-    if ($courseResult->num_rows > 0) {
-        $courseData = $courseResult->fetch_assoc();
-        $title = $courseData['title'];
-        $description = $courseData['description'];
-        $creator_id = $courseData['creator_id'];
+    // ... (reszta kodu bez zmian)
 
-        // Pobierz imię twórcy na podstawie ID
-        $selectCreatorSql = "SELECT username FROM users WHERE id=$creator_id";
-        $creatorResult = $conn->query($selectCreatorSql);
+    // Sprawdź, czy użytkownik jest zalogowany
+    if (isset($_SESSION['user_id'])) {
+        // Sprawdź, czy użytkownik ukończył kurs
+        $checkCompletionSql = "SELECT * FROM completed_courses WHERE user_id={$_SESSION['user_id']} AND course_id={$course_id}";
+        $result = $conn->query($checkCompletionSql);
 
-        if ($creatorResult->num_rows > 0) {
-            $creatorData = $creatorResult->fetch_assoc();
-            $creator_name = $creatorData['username'];
+        include '../includes/header.php';
+
+        if ($result->num_rows > 0) {
+            // Użytkownik ukończył kurs, umożliwiamy ocenę
+            echo '<div class="container">';
+            echo '<h2>Przeglądaj Kurs</h2>';
+            echo '<p>Tytuł: ' . $title . '</p>';
+            echo '<p>Opis: ' . $description . '</p>';
+            echo '<p>Twórca: ' . $creator_name . '</p>';
+            echo '<p>Średnia ocena: ' . $average_rating . '</p>';
+            echo '<h3>Oceny kursu:</h3>';
+            echo '<ul>';
+            if (!empty($ratings)) {
+                foreach ($ratings as $rating) {
+                    echo '<li>' . $rating . '</li>';
+                }
+            } else {
+                echo '<li>Brak ocen.</li>';
+            }
+            echo '</ul>';
+
+            // Dodaj przycisk "Rozpocznij Kurs Ponownie"
+            echo '<h3>Oceń kurs:</h3>';
+            echo '<form method="post" action="../actions/rate_course_process.php">';
+            echo '<input type="hidden" name="course_id" value="' . $course_id . '">';
+            echo '<label for="rating">Wybierz ocenę od 1 do 5:</label>';
+            echo '<select name="rating" required>';
+            echo '<option value="1">1 gwiazdka</option>';
+            echo '<option value="2">2 gwiazdki</option>';
+            echo '<option value="3">3 gwiazdki</option>';
+            echo '<option value="4">4 gwiazdki</option>';
+            echo '<option value="5">5 gwiazdek</option>';
+            echo '</select>';
+            echo '<button type="submit">Oceń</button>';
+            echo '</form>';
+
+            // Dodaj przycisk "Rozpocznij Kurs Ponownie"
+            echo '<form method="post" action="/phpsql/actions/start_course.php">';
+            echo '<input type="hidden" name="course_id" value="' . $course_id . '">';
+            echo '<button type="submit">Rozpocznij Kurs Ponownie</button>';
+            echo '</form>';
+        } else {
+            // Użytkownik nie ukończył kursu, przekieruj na stronę rozpoczęcia kursu
+            echo '<div class="container">';
+            echo '<h2>Przeglądaj Kurs</h2>';
+            echo '<p>Tytuł: ' . $title . '</p>';
+            echo '<p>Opis: ' . $description . '</p>';
+            echo '<p>Twórca: ' . $creator_name . '</p>';
+            echo '<p>Średnia ocena: ' . $average_rating . '</p>';
+            echo '<h3>Oceny kursu:</h3>';
+            echo '<ul>';
+            if (!empty($ratings)) {
+                foreach ($ratings as $rating) {
+                    echo '<li>' . $rating . '</li>';
+                }
+            } else {
+                echo '<li>Brak ocen.</li>';
+            }
+            echo '</ul>';
+
+            // Dodaj przycisk "Rozpocznij Kurs"
+            echo '<form method="post" action="/phpsql/actions/start_course.php">';
+            echo '<input type="hidden" name="course_id" value="' . $course_id . '">';
+            echo '<button type="submit">Rozpocznij Kurs</button>';
+            echo '</form>';
         }
-    }
 
-    // Pobierz średnią ocenę kursu na podstawie ID kursu
-    $selectAvgRatingSql = "SELECT AVG(rating) as avg_rating FROM course_ratings WHERE course_id=$course_id";
-    $avgRatingResult = $conn->query($selectAvgRatingSql);
-
-    if ($avgRatingResult->num_rows > 0) {
-        $avgRatingData = $avgRatingResult->fetch_assoc();
-        $average_rating = $avgRatingData['avg_rating'];
-    }
-
-    // Pobierz oceny kursu na podstawie ID kursu
-    $selectRatingsSql = "SELECT * FROM course_ratings WHERE course_id=$course_id";
-    $ratingsResult = $conn->query($selectRatingsSql);
-
-    $ratings = array(); // Inicjalizacja tablicy do przechowywania ocen
-
-    if ($ratingsResult->num_rows > 0) {
-        while ($ratingData = $ratingsResult->fetch_assoc()) {
-            $ratings[] = $ratingData['rating'];
-        }
+        echo '</div>'; // Zamknij kontener
+        include '../includes/footer.php';
+    } else {
+        // Użytkownik nie jest zalogowany, przekieruj na stronę logowania
+        header("Location: /phpsql/pages/login.php");
+        exit;
     }
 }
-
-include '../includes/header.php';
 ?>
-
-<div class="container">
-    <h2>Przeglądaj Kurs</h2>
-    <?php
-    // Wyświetl dane
-    echo '<p>Tytuł: ' . $title . '</p>';
-    echo '<p>Opis: ' . $description . '</p>';
-    echo '<p>Twórca: ' . $creator_name . '</p>';
-
-    // Wyświetl dane
-    echo '<p>Średnia ocena: ' . $average_rating . '</p>';
-
-    // Wyświetl oceny
-    echo '<h3>Oceny kursu:</h3>';
-    echo '<ul>';
-    if (!empty($ratings)) {
-        foreach ($ratings as $rating) {
-            echo '<li>' . $rating . '</li>';
-        }
-    } else {
-        echo '<li>Brak ocen.</li>';
-    }
-    echo '</ul>';
-
-    // Dodaj formularz oceny tylko dla zalogowanych użytkowników
-    if (isset($_SESSION['user_id'])) {
-        echo '<h3>Oceń kurs:</h3>';
-        echo '<form method="post" action="../actions/rate_course_process.php">';
-        echo '<input type="hidden" name="course_id" value="' . $course_id . '">';
-        echo '<label for="rating">Wybierz ocenę od 1 do 5:</label>';
-        echo '<select name="rating" required>';
-        echo '<option value="1">1 gwiazdka</option>';
-        echo '<option value="2">2 gwiazdki</option>';
-        echo '<option value="3">3 gwiazdki</option>';
-        echo '<option value="4">4 gwiazdki</option>';
-        echo '<option value="5">5 gwiazdek</option>';
-        echo '</select>';
-        echo '<button type="submit">Oceń</button>';
-        echo '</form>';
-    }
-    ?>
-</div>
-
-<?php include '../includes/footer.php'; ?>
