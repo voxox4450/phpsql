df4b4cb9b940c239c4d1f0b7d3866b4e6b224c06
Jakub Malik <malik.j@poczta.fm>
2023-11-29 12:20:56 +0100

    s

--- actions/edit_course_process.php
+++ actions/edit_course_process.php
@@ -2,12 +2,6 @@
 // edit_course_process.php
 session_start();
 
-// Sprawdzenie, czy użytkownik jest zalogowany
-if (!isset($_SESSION['username'])) {
-    header("Location: /phpsql/pages/login.php");
-    exit;
-}
-
 include('../settings.php');
 
 // Obsługa edycji kursu
@@ -18,8 +12,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Aktualizacja danych kursu w bazie danych
     $updateCourseSql = "UPDATE courses SET title='$title', description='$description' WHERE id=$course_id";
 
-    // Pomyślna edycja kursu, przekieruj na stronę z zarządzaniem kursami
-    header("Location: manage_courses.php");
+    if ($conn->query($updateCourseSql) === TRUE) {
+        // Pomyślna edycja kursu, przekieruj na stronę z zarządzaniem kursami
+        header("Location: /phpsql/pages/manage_courses.php");
+    } else {
+        // Błąd edycji kursu, przekieruj na stronę z zarządzaniem kursami z komunikatem błędu
+        header("Location: /phpsql/pages/manage_courses.php?error=" . urlencode("Błąd edycji kursu: " . $conn->error));
+    }
 }
 
 $conn->close();
