7120a142f90ead54ec507cdad95b7f14ed5500bd
voxox4450 <voxoxgaming@gmail.com>
2023-11-29 21:38:00 +0100

    password

--- actions/login_process.php
+++ actions/login_process.php
@@ -1,6 +1,5 @@
 <?php
 // Połączenie z bazą danych
-
 include('../settings.php');
 
 if ($conn->connect_error) {
@@ -11,22 +10,36 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-    $sql = "SELECT id, username FROM users WHERE username='$username' AND password='$hashed_password'";
-    $result = $conn->query($sql);
+    // Zabezpieczenie przed SQL Injection
+    $sql = "SELECT id, username, password FROM users WHERE username=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
     if ($result->num_rows > 0) {
-        // Użytkownik zalogowany, przekieruj na stronę główną
-        session_start();
         $userData = $result->fetch_assoc();
-        $_SESSION['user_id'] = $userData['id'];  // Przechowaj ID użytkownika w sesji
-        $_SESSION['username'] = $userData['username'];  // Ustawienie danych sesji
-        header("Location: /phpsql/pages/profile.php");
+        $hashed_password = $userData['password'];
+
+        // Porównanie hasła za pomocą password_verify
+        if (password_verify($password, $hashed_password)) {
+            // Użytkownik zalogowany, przekieruj na stronę główną
+            session_start();
+            $_SESSION['user_id'] = $userData['id'];  // Przechowaj ID użytkownika w sesji
+            $_SESSION['username'] = $userData['username'];  // Ustawienie danych sesji
+            header("Location: /phpsql/pages/profile.php");
+            exit; // Zakończ po przekierowaniu
+        } else {
+            // Błąd logowania
+            echo "Błędna nazwa użytkownika lub hasło.";
+        }
     } else {
         // Błąd logowania
         echo "Błędna nazwa użytkownika lub hasło.";
     }
+
+    $stmt->close(); // Zamknięcie prepared statement
 }
 
 $conn->close();
