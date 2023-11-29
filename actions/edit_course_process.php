b96c2c870d236346e321eee701f843a5d5853719
Jakub Malik <malik.j@poczta.fm>
2023-11-29 12:57:04 +0100

    s

--- actions/edit_course_process.php
+++ actions/edit_course_process.php
@@ -1,36 +1,38 @@
 <?php
 // edit_course_process.php
-session_start();
 
-// Sprawdzenie, czy użytkownik jest zalogowany
-if (!isset($_SESSION['username'])) {
-    header("Location: /phpsql/pages/login.php");
-    exit;
-}
-
-include('../settings.php');
-// Obsługa edycji kursu
+// Sprawdź, czy formularz został przesłany
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $course_id = $_POST['course_id'];
-    $title = $_POST['title'];
-    $description = $_POST['description'];
-
-    // Aktualizacja danych kursu w bazie danych przy użyciu prepared statements
-    $updateCourseSql = "UPDATE courses SET title=?, description=? WHERE id=?";
     
-    $stmt = $conn->prepare($updateCourseSql);
-    $stmt->bind_param("ssi", $title, $description, $course_id);
-
-    if ($stmt->execute()) {
-        // Pomyślna edycja kursu, przekieruj na stronę z zarządzaniem kursami
-        header("Location: /phpsql/pages/manage_courses.php");
-    } else {
-        // Błąd edycji kursu, przekieruj na stronę z zarządzaniem kursami z komunikatem błędu
-        header("Location: /phpsql/pages/manage_courses.php?error=" . urlencode("Błąd edycji kursu: " . $stmt->error));
+    // Sprawdź, czy użytkownik jest zalogowany
+    session_start();
+    if (!isset($_SESSION['username'])) {
+        header("Location: /phpsql/pages/login.php");
+        exit;
     }
 
-    $stmt->close();
-}
+    // Pobierz dane z formularza
+    $course_id = $_POST["course_id"];
+    $title = $_POST["title"];
+    $description = $_POST["description"];
 
-$conn->close();
+
+    include('../settings.php');
+
+    // Zaktualizuj dane kursu w bazie danych
+    $sql = "UPDATE courses SET title='$title', description='$description' WHERE id=$course_id";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Zmiany zostały zapisane pomyślnie";
+    } else {
+        echo "Błąd podczas zapisywania zmian: " . $conn->error;
+    }
+
+    // Zamknij połączenie z bazą danych
+    $conn->close();
+} else {
+    // Przekieruj użytkownika w przypadku próby dostępu bez przesłania formularza
+    header("Location: /phpsql/pages/user_panel.php");
+    exit;
+}
 ?>
