6c6d217263f3fcc9b11786428f483e77adaa0430
Jakub Malik <malik.j@poczta.fm>
2023-11-29 22:41:43 +0100

    s

--- actions/register_process.php
+++ actions/register_process.php
@@ -10,57 +10,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Sprawdzenie, czy użytkownik o podanej nazwie już istnieje
     $check_user_sql = "SELECT * FROM users WHERE username=?";
     $stmt = $conn->prepare($check_user_sql);
-
-    if ($stmt === false) {
-        die('Error: ' . htmlspecialchars($conn->error));
-    }
-
     $stmt->bind_param("s", $username);
     $stmt->execute();
     $check_user_result = $stmt->get_result();
 
     if ($check_user_result === false) {
         // Błąd zapytania SQL
-        echo "Błąd zapytania SQL: " . $stmt->error;
+        echo "Błąd zapytania SQL: " . $conn->error;
     } elseif ($check_user_result->num_rows > 0) {
         // Użytkownik o tej nazwie już istnieje
-        $error_message = "Użytkownik o podanej nazwie już istnieje.";
-        include '../pages/register.php';  // Dodaj tę linię
-        exit;  // Dodaj tę linię
+        $_SESSION['registration_message'] = "Użytkownik o podanej nazwie już istnieje.";
     } else {
         // Prosta walidacja hasła (przynajmniej 8 znaków)
         if (strlen($password) < 8) {
-            $error_message = "Hasło musi mieć co najmniej 8 znaków.";
-            include '../pages/register.php';  // Dodaj tę linię
-            exit;  // Dodaj tę linię
+            $_SESSION['registration_message'] = "Hasło musi mieć co najmniej 8 znaków.";
+            exit;
+        }
+
+        // Haszowanie hasła przed zapisaniem do bazy danych
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+        // Dodanie nowego użytkownika do bazy danych (parametryzowane zapytanie)
+        $insert_user_sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $stmt = $conn->prepare($insert_user_sql);
+        $stmt->bind_param("ss", $username, $hashed_password);
+        
+        if ($stmt->execute()) {
+            // Pomyślna rejestracja, przekieruj na stronę logowania
+            header("Location:/phpsql/pages/login.php");
         } else {
-            // Haszowanie hasła przed zapisaniem do bazy danych
-            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-            // Dodanie nowego użytkownika do bazy danych (parametryzowane zapytanie)
-            $insert_user_sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-            $stmt = $conn->prepare($insert_user_sql);
-
-            if ($stmt === false) {
-                die('Error: ' . htmlspecialchars($conn->error));
-            }
-
-            $stmt->bind_param("ss", $username, $hashed_password);
-            
-            if ($stmt->execute()) {
-                // Pomyślna rejestracja, przekieruj na stronę logowania
-                header("Location:/phpsql/pages/login.php");
-                exit;
-            } else {
-                // Błąd rejestracji
-                $error_message = "Błąd rejestracji: " . $stmt->error;
-                include '../pages/register.php';  // Dodaj tę linię
-                exit;  // Dodaj tę linię
-            }
+            // Błąd rejestracji
+            $_SESSION['registration_message'] = "Błąd rejestracji: " . $conn->error;
         }
     }
 
-    $stmt->close();
+    $stmt->close(); // Zamknięcie prepared statement
 }
 
 $conn->close();
--- pages/register.php
+++ pages/register.php
@@ -1,3 +1,13 @@
+<?php
+session_start(); // Rozpoczęcie sesji
+
+// Sprawdzenie, czy istnieje komunikat w sesji
+$registration_message = isset($_SESSION['registration_message']) ? $_SESSION['registration_message'] : '';
+
+// Usunięcie komunikatu z sesji, aby znikł po odświeżeniu strony
+unset($_SESSION['registration_message']);
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -5,6 +15,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <title>Rejestracja</title>
+    <style>
+        /* Dodaj odpowiednie style dla komunikatu */
+        .registration-message {
+            color: red; /* Kolor komunikatu o błędzie */
+            font-weight: bold;
+            /* Dodaj inne stylizacje według potrzeb */
+        }
+    </style>
 </head>
 <body>
     <?php include '../includes/header.php'; ?>
@@ -12,6 +30,12 @@
     <div class="container">
         <h2>Zarejestruj się</h2>
 
+        <?php
+        // Wyświetlenie komunikatu
+        if (!empty($registration_message)) {
+            echo '<p class="registration-message">' . $registration_message . '</p>';
+        }
+        ?>
 
         <form action="/phpsql/actions/register_process.php" method="post">
             <label for="username">Nazwa użytkownika:</label>
@@ -23,13 +47,6 @@
             <button type="submit">Zarejestruj się</button>
         </form>
     </div>
-
-    <?php
-        // Wyświetlanie komunikatu o błędzie, jeśli istnieje
-        if (isset($error_message)) {
-            echo '<p class="error">' . $error_message . '</p>';
-        }
-        ?>
 
     <?php include '../includes/footer.php'; ?>
 </body>
