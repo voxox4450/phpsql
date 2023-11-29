34a6f0a9c4c455cf274852bc00c3a2e509fcc04b
Jakub Malik <malik.j@poczta.fm>
2023-11-29 21:09:06 +0100

    s

--- pages/user_panel.php
+++ pages/user_panel.php
@@ -28,12 +28,37 @@ $username = $_SESSION['username'];
 
     <div class="container">
         <h2>Witaj, <?php echo $username; ?>, w Twoim Panelu Użytkownika!</h2>
+
         <!-- Dodaj linki do różnych funkcji dostępnych dla zalogowanego użytkownika -->
         <ul>
             <li><a href="/phpsql/pages/manage_courses.php">Zarządzak swoimi kursami</a></li>
             <li><a href="/phpsql/pages/edit_profile.php">Edytuj Profil</a></li>
             <li><a href="/phpsql/pages/change_password.php">Zmień Hasło</a></li>
         </ul>
+
+        <!-- Dodaj sekcję wyświetlającą ukończone kursy danego uczestnika -->
+        <h3>Twoje ukończone kursy:</h3>
+        <?php
+        // Pobierz ukończone kursy z bazy danych
+        $user_id = $_SESSION['user_id'];
+
+        $completedCoursesSql = "SELECT courses.title, completed_courses.completion_date
+                               FROM courses
+                               INNER JOIN completed_courses ON courses.id = completed_courses.course_id
+                               WHERE completed_courses.user_id = $user_id";
+
+        $completedCoursesResult = $conn->query($completedCoursesSql);
+
+        if ($completedCoursesResult->num_rows > 0) {
+            echo '<ul>';
+            while ($row = $completedCoursesResult->fetch_assoc()) {
+                echo '<li>' . $row['title'] . ' - Data ukończenia: ' . $row['completion_date'] . '</li>';
+            }
+            echo '</ul>';
+        } else {
+            echo 'Nie ukończyłeś jeszcze żadnych kursów.';
+        }
+        ?>
     </div>
 
     <?php include '../includes/footer.php'; ?>
