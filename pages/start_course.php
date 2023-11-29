7dc39065ed7b990cf106fb6939f1f24e5c132d0b
Jakub Malik <malik.j@poczta.fm>
2023-11-29 20:46:17 +0100

    s

--- pages/start_course.php
+++ pages/start_course.php
@@ -9,36 +9,42 @@ if (!isset($_SESSION['user_id'])) {
     exit;
 }
 
-// Pobierz ID kursu z parametru w URL
-if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['id'])) {
-    $course_id = $_GET['id'];
-
-    // Pobierz dane kursu na podstawie ID
-    $selectCourseSql = "SELECT * FROM courses WHERE id=$course_id";
-    $courseResult = $conn->query($selectCourseSql);
-
-    if ($courseResult->num_rows > 0) {
-        $courseData = $courseResult->fetch_assoc();
-        $title = $courseData['title'];
-    }
-
-    include '../includes/header.php';
-
-    echo '<div class="container">';
-    echo '<h2>Przeglądaj Kurs</h2>';
-    echo '<p>Tytuł: ' . $title . '</p>';
- 
-
-    // Dodaj przycisk "Rozpocznij Kurs"
-    echo '<form method="post" action="/phpsql/pages/start_course_process.php">';
-    echo '<input type="hidden" name="course_id" value="' . $course_id . '">';
-    echo '<button type="submit">Ukończ kurs</button>';
-    echo '</form>';
-    
-
-    echo '</div>'; // Zamknij kontener
-    include '../includes/footer.php';
-} else {
-    echo "Błędne dane przesłane do formularza.";s
+// Sprawdź, czy istnieje parametr 'id' w tablicy $_GET
+if (!isset($_GET['id'])) {
+    echo "Błędne dane przesłane do formularza.";
+    exit;
 }
+
+// Pobierz ID kursu z parametru w URL
+$course_id = $_GET['id'];
+
+// Pobierz dane kursu na podstawie ID
+$selectCourseSql = "SELECT * FROM courses WHERE id=?";
+$stmt = $conn->prepare($selectCourseSql);
+$stmt->bind_param("i", $course_id);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    $courseData = $result->fetch_assoc();
+    $title = $courseData['title'];
+} else {
+    echo "Brak kursu o podanym ID.";
+    exit;
+}
+
+include '../includes/header.php';
+
+echo '<div class="container">';
+echo '<h2>Przeglądaj Kurs</h2>';
+echo '<p>Tytuł: ' . $title . '</p>';
+
+// Dodaj przycisk "Ukończ kurs"
+echo '<form method="post" action="/phpsql/pages/start_course_process.php">';
+echo '<input type="hidden" name="course_id" value="' . $course_id . '">';
+echo '<button type="submit">Ukończ kurs</button>';
+echo '</form>';
+
+echo '</div>'; // Zamknij kontener
+include '../includes/footer.php';
 ?>
