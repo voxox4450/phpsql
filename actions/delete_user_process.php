73c7fe7d1ef35388a42d35a3efd54fb4727b467d
Jakub Malik <malik.j@poczta.fm>
2023-12-01 21:03:22 +0100

    s

--- actions/delete_user_process.php
+++ actions/delete_user_process.php
@@ -21,48 +21,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['user_id'])) {
         exit;
     }
 
-    // Przygotuj i wykonaj zapytanie do usuwania ocen kursów utworzonych przez użytkownika
-    $deleteUserCourseRatingsSql = "DELETE cr FROM course_ratings cr 
-                                    JOIN courses c ON cr.course_id = c.id
-                                    WHERE c.creator_id = ?";
-    $stmtUserCourseRatings = $conn->prepare($deleteUserCourseRatingsSql);
-    $stmtUserCourseRatings->bind_param("i", $user_id);
-    if (!$stmtUserCourseRatings->execute()) {
-        echo "Błąd usuwania ocen kursów utworzonych przez użytkownika: " . $stmtUserCourseRatings->error;
-        exit;
-    }
-    $stmtUserCourseRatings->close();
-
-    // Przygotuj i wykonaj zapytanie do usuwania ocen kursów wystawionych przez użytkownika
-    $deleteUserRatingsSql = "DELETE FROM course_ratings WHERE user_id = ?";
-    $stmtUserRatings = $conn->prepare($deleteUserRatingsSql);
-    $stmtUserRatings->bind_param("i", $user_id);
-    if (!$stmtUserRatings->execute()) {
-        echo "Błąd usuwania ocen kursów wystawionych przez użytkownika: " . $stmtUserRatings->error;
-        exit;
-    }
-    $stmtUserRatings->close();
-
-    // Przygotuj i wykonaj zapytanie do usuwania zależności od użytkownika w tabeli completed_courses
-    $deleteCompletedCoursesSql = "DELETE FROM completed_courses WHERE user_id = ?";
-    $stmtCompletedCourses = $conn->prepare($deleteCompletedCoursesSql);
-    $stmtCompletedCourses->bind_param("i", $user_id);
-    if (!$stmtCompletedCourses->execute()) {
-        echo "Błąd usuwania zależności od użytkownika w tabeli completed_courses: " . $stmtCompletedCourses->error;
-        exit;
-    }
-    $stmtCompletedCourses->close();
-
-    // Przygotuj i wykonaj zapytanie do usuwania kursów utworzonych przez użytkownika
-    $deleteUserCoursesSql = "DELETE FROM courses WHERE creator_id = ?";
-    $stmtUserCourses = $conn->prepare($deleteUserCoursesSql);
-    $stmtUserCourses->bind_param("i", $user_id);
-    if (!$stmtUserCourses->execute()) {
-        echo "Błąd usuwania kursów utworzonych przez użytkownika: " . $stmtUserCourses->error;
-        exit;
-    }
-    $stmtUserCourses->close();
-
     // Przygotuj i wykonaj zapytanie do usuwania użytkownika z bazy danych
     $deleteUserSql = "DELETE FROM users WHERE id = ?";
     $stmt = $conn->prepare($deleteUserSql);
