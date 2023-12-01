dc468c73830cee80a6f394a42eb5038291a849fe
Jakub Malik <malik.j@poczta.fm>
2023-12-01 19:40:51 +0100

    s

--- pages/user_panel.php
+++ pages/user_panel.php
@@ -34,29 +34,6 @@ echo "Rola użytkownika: " . $_SESSION['role'];
             <li><a href="/phpsql/pages/change_password.php">Zmień Hasło</a></li>
         </ul>
 
-        <!-- Dodaj sekcję wyświetlającą ukończone kursy danego uczestnika -->
-        <h3>Twoje ukończone kursy:</h3>
-        <?php
-        // Pobierz ukończone kursy z bazy danych
-        $user_id = $_SESSION['user_id'];
-
-        $completedCoursesSql = "SELECT courses.title, completed_courses.completion_date
-                               FROM courses
-                               INNER JOIN completed_courses ON courses.id = completed_courses.course_id
-                               WHERE completed_courses.user_id = $user_id";
-
-        $completedCoursesResult = $conn->query($completedCoursesSql);
-
-        if ($completedCoursesResult->num_rows > 0) {
-            echo '<ul>';
-            while ($row = $completedCoursesResult->fetch_assoc()) {
-                echo '<li>' . $row['title'] . ' - Data ukończenia: ' . $row['completion_date'] . '</li>';
-            }
-            echo '</ul>';
-        } else {
-            echo 'Nie ukończyłeś jeszcze żadnych kursów.';
-        }
-        ?>
     </div>
 
     <?php include '../includes/footer.php'; ?>
