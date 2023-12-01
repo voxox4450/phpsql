69d71e0cce678f0895c59f7012aac4ec0a6f7501
voxox4450 <voxoxgaming@gmail.com>
2023-12-01 20:16:05 +0100

    srednik xd

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,7 @@
             <ul>
                 <li><a href="/phpsql/index.php">Strona główna</a></li>
                 <?php
-                $role = $_SESSION['role']
+                $role = $_SESSION['role'];
                 if (isset($_SESSION['username'])) {
                     if ($_SESSION['role'] == 'admin') {
                         // Jeśli użytkownik jest administratorem, wyświetl panel administratora
