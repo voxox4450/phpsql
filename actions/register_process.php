c10d3275d791bd40be9680837a4995de98843da9
Jakub Malik <malik.j@poczta.fm>
2023-12-01 21:33:51 +0100

    register walidacja

--- actions/register_process.php
+++ actions/register_process.php
@@ -28,22 +28,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (strlen($password) < 8) {
             $_SESSION['registration_message'] = "Hasło musi mieć co najmniej 8 znaków.";
         } else {
-            // Haszowanie hasła przed zapisaniem do bazy danych
-            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-            // Dodanie nowego użytkownika do bazy danych (parametryzowane zapytanie)
-            $insert_user_sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-            $stmt = $conn->prepare($insert_user_sql);
-            $stmt->bind_param("ss", $username, $hashed_password);
-            
-            if ($stmt->execute()) {
-                // Pomyślna rejestracja, przekieruj na stronę logowania
-                $_SESSION['registration_message'] = "Pomyślna rejestracja. Możesz się teraz zalogować.";
-                header("Location:/phpsql/pages/login.php");
-                exit; // Dodaj exit po przekierowaniu
+            // Dodatkowa walidacja dla nazwy użytkownika
+            if (strlen($username) < 5) {
+                $_SESSION['registration_message'] = "Nazwa użytkownika musi mieć co najmniej 5 znaków.";
             } else {
-                // Błąd rejestracji
-                $_SESSION['registration_message'] = "Błąd rejestracji: " . $conn->error;
+                // Dodatkowa walidacja hasła
+                if (!preg_match('/[A-Z]/', $password) ||    // Duża litera
+                    !preg_match('/[a-z]/', $password) ||    // Mała litera
+                    !preg_match('/\d/', $password) ||        // Cyfra
+                    !preg_match('/[^A-Za-z0-9]/', $password) // Znak specjalny
+                ) {
+                    $_SESSION['registration_message'] = "Hasło musi zawierać co najmniej jedną dużą literę, jedną małą literę, jedną cyfrę i jeden znak specjalny.";
+                } else {
+                    // Haszowanie hasła przed zapisaniem do bazy danych
+                    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+                    // Dodanie nowego użytkownika do bazy danych (parametryzowane zapytanie)
+                    $insert_user_sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+                    $stmt = $conn->prepare($insert_user_sql);
+                    $stmt->bind_param("ss", $username, $hashed_password);
+
+                    if ($stmt->execute()) {
+                        // Pomyślna rejestracja, przekieruj na stronę logowania
+                        $_SESSION['registration_message'] = "Pomyślna rejestracja. Możesz się teraz zalogować.";
+                        header("Location:/phpsql/pages/login.php");
+                        exit; // Dodaj exit po przekierowaniu
+                    } else {
+                        // Błąd rejestracji
+                        $_SESSION['registration_message'] = "Błąd rejestracji: " . $conn->error;
+                    }
+                }
             }
         }
     }
@@ -52,7 +66,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Przekieruj z powrotem na stronę logowania
     header("Location: /phpsql/pages/register.php");
     exit;
-    
 }
 
 $conn->close();
