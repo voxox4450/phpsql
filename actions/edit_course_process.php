70db164abd1bbe7dba89944f5c92820e4a6ec8fb
Jakub Malik <malik.j@poczta.fm>
2023-12-01 22:30:15 +0100

    s

--- actions/edit_course_process.php
+++ actions/edit_course_process.php
@@ -33,13 +33,50 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $courseData = $result->fetch_assoc();
         $creator_id = $courseData['creator_id'];
 
-        // Zaktualizuj dane kursu w bazie danych
-        $updateSql = "UPDATE courses SET title='$title', description='$description' WHERE id=$course_id";
+        // Sprawdź, czy nie istnieje już kurs o podanym tytule i opisie (oprócz aktualnie edytowanego)
+        $checkDuplicateSql = "SELECT * FROM courses WHERE title = ? AND description = ? AND id <> ?";
+        $stmtCheckDuplicate = $conn->prepare($checkDuplicateSql);
 
-        if ($conn->query($updateSql) === TRUE) {
-            echo "Zmiany zostały zapisane pomyślnie";
+        if (!$stmtCheckDuplicate) {
+            die("Prepared statement error (check duplicate): " . $conn->error);
+        }
+
+        $stmtCheckDuplicate->bind_param("ssi", $title, $description, $course_id);
+        $stmtCheckDuplicate->execute();
+        $stmtCheckDuplicate->store_result();
+
+        if ($stmtCheckDuplicate->num_rows > 0) {
+            // Kurs o podanym tytule i opisie już istnieje (oprócz aktualnie edytowanego)
+            $stmtCheckDuplicate->close();
+            $error_message = "Kurs o podanym tytule i opisie już istnieje.";
+            header("Location: /phpsql/pages/edit_course.php?id=$course_id&error=" . urlencode($error_message));
+            exit;
+        }
+
+        // Zamykamy prepared statement
+        $stmtCheckDuplicate->close();
+
+        // Zaktualizuj dane kursu w bazie danych
+        $updateSql = "UPDATE courses SET title=?, description=? WHERE id=?";
+        $stmtUpdate = $conn->prepare($updateSql);
+
+        if (!$stmtUpdate) {
+            die("Prepared statement error (update): " . $conn->error);
+        }
+
+        $stmtUpdate->bind_param("ssi", $title, $description, $course_id);
+
+        if ($stmtUpdate->execute()) {
+            // Pomyślne zaktualizowanie kursu, przekieruj na stronę z zarządzaniem kursami
+            $stmtUpdate->close();
+            header("Location: /phpsql/pages/manage_courses.php");
+            exit;
         } else {
-            echo "Błąd podczas zapisywania zmian: " . $conn->error;
+            // Błąd podczas aktualizacji kursu
+            $stmtUpdate->close();
+            $error_message = "Błąd podczas aktualizacji kursu. Spróbuj ponownie.";
+            header("Location: /phpsql/pages/edit_course.php?id=$course_id&error=" . urlencode($error_message));
+            exit;
         }
 
     } else {
